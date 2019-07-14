f4e09a0ac18e94bbbce4c540819e5ad59e6f2cdb
Thomas <thomy.ripari@outlook.com>
2019-07-14 19:05:20 -0300

    Calculator Design #1

--- App.js
+++ App.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>HELLO WORLD</Text>
+      <View style={styles.container}>
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+          <View style={styles.operations}></View>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  result: {
+    flex: 2,
+    backgroundColor: 'red'
+  },
+
+  calculation: {
+    flex: 1,
+    backgroundColor: 'green'
+  },
+
+  buttons: {
+    flex: 7,
+    flexDirection: 'row'
+  },
+
+  numbers: {
+    flex: 2,
+    backgroundColor: 'black'
+  },
+
+  operations: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  }
 
 });
